fc941ca9c00381c11ff71eb99370aeb1c65633ed
Krebkel <medvedev.andrew@mail.com>
2024-07-13 23:25:33 +0300

    Added web app base

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,6 +1,62 @@
-namespace Web;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+using Data;
+using Passwords;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddControllers();
+
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen();
+
+builder.Services
+    .Configure<DataOptions>(builder.Configuration.GetSection("Postgres"));
+
+builder.Services
+    .AddPostgresData();
+
+builder.Services
+    .AddPostgresPasswordEntries();
+
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+// Swagger UI и HealthChecks
+app.UseRouting();
+app.UseEndpoints(endpoints =>
 {
+    endpoints.MapControllers();
+});
+
+// Swagger UI
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseDefaultFiles()
+    .UseStaticFiles(new StaticFileOptions
+    {
+        OnPrepareResponse = context =>
+        {
+            var headers = context.Context.Response.GetTypedHeaders();
+            headers.CacheControl = new CacheControlHeaderValue { Public = true, MaxAge = TimeSpan.FromMinutes(1) };
+        }
+    });
+
+InitializeDatabase(app);
+app.Run();
+
+void InitializeDatabase(IApplicationBuilder application)
+{
+    using var scope = application.ApplicationServices
+        .GetRequiredService<IServiceScopeFactory>()
+        .CreateScope();
     
+    scope.ServiceProvider.GetRequiredService<AppDbContext>().Database.Migrate();
 }
